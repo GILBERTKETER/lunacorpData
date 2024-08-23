6468f94d986876021154c21c556ac6c76fda8797
gilbertketer <116883167+GILBERTKETER@users.noreply.github.com>
2024-08-23 19:06:22 +0300

    Update resources.php

--- resources.php
+++ resources.php
@@ -524,6 +524,18 @@ $is_admin = ($user_type === 'administrator');
   <script src="js/bootstrap.min.js"></script>
   <!-- Main JS -->
   <script src="js/main.js"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1380165038596121"
+     crossorigin="anonymous"></script>
+<!-- lunacorpdata -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1380165038596121"
+     data-ad-slot="5400503743"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 </body>
 
 </html>
